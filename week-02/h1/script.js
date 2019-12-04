3c0ab7a0cf186a09ae494e8a5e58d12d5e578e93
Ari Tserendavaa <tserenda@hotmail.com>
2019-12-03 20:24:14 -0800

    Added stylesheet

--- week-02/h1/script.js
+++ week-02/h1/script.js
@@ -1,24 +1,40 @@
-var passwordLength = 0;
-while (passwordLength < 8 || passwordLength > 128) {
-  passwordLength = prompt("Enter password length between 8 and 128");
-}
+var generateButton = document.querySelector("#generate");
+var copyButton = document.querySelector("#copy");
 
-var includeSpecialChars = confirm("Include special characters?");
-var includeUppercase = confirm("Include uppercase?");
-var includeLowercase = confirm("Include lowercase?");
-var includeDigits = confirm("Include digits?");
+generateButton.addEventListener("click", function() {
+  var passwordLength = 0;
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Enter password length between 8 and 128");
+  }
 
-var charSet = "";
-if (includeUppercase) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-if (includeLowercase) charSet += "abcdefghijklmnopqrstuvwxyz";
-if (includeSpecialChars) charSet += "!#$%&'()*+,-./:;<=>?@{|}~";
-if (includeDigits) charSet += "0123456789";
+  var [includeSpecialChars, includeUppercaseChars, includeLowercaseChars, includeDigits] = [false, false, false, false];
+  while (!includeSpecialChars && !includeUppercaseChars && !includeLowercaseChars && !includeDigits) {
+    includeSpecialChars = confirm("Include special characters?");
+    includeUppercaseChars = confirm("Include UPPERCASE?");
+    includeLowercaseChars = confirm("Include lowercase?");
+    includeDigits = confirm("Include digits?");
+  }
 
-var password = "";
-for (let i = 0; i < passwordLength; i++) {
-  const random = (Math.random() * 100) % charSet.length;
-  password += charSet.charAt(random);
-}
+  var charSet = "";
+  if (includeUppercaseChars) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercaseChars) charSet += "abcdefghijklmnopqrstuvwxyz";
+  if (includeSpecialChars) charSet += "!#$%&'()*+,-./:;<=>?@{|}~";
+  if (includeDigits) charSet += "0123456789";
 
-var textArea = document.querySelector("#password");
-textArea.textContent = password;
+  var password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = (Math.random() * 100) % charSet.length;
+    password += charSet.charAt(randomIndex);
+  }
+  var textAreaElement = document.querySelector("#password");
+  textAreaElement.textContent = password;
+  copyButton.disabled = false;
+});
+
+copyButton.addEventListener("click", function() {
+  var password = document.querySelector("#password");
+  password.select();
+  password.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Password copied:\n" + password.value);
+});
